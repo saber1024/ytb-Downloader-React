cf5fceb44cfe626c6fb061b380d9723a83898cd4
zhu <zhushuai19@gmail.com>
2020-11-03 16:37:10 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
-// import Header from "./Componets/Header";
-// import HomeScreen from "./Screens/HomeScreen";
+import Header from "./Componets/Header";
+import HomeScreen from "./Screens/HomeScreen";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 function App() {
